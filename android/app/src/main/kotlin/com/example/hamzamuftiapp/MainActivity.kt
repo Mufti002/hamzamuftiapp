c30b621b2fb13e0fb383996c11810d7479d8a7a8
hamza <hamzamufti.2002@gmail.com>
2021-05-12 06:31:58 +0500

    hamzamuftiapp

---
+++ android/app/src/main/kotlin/com/example/hamzamuftiapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hamzamuftiapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: (Scaffold(
+        body: Text(
+            "Name:MUHAMMAD HAMZA MUFTI\nemail-id:hamzamufti.2002@gmail.com"),
+      )),
+    );
+  }
+}
